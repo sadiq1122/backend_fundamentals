73aa616f4f559cc86088464156b57677fc034f80
sadiq <sadiq.mohammad@paysense.in>
2024-03-29 14:10:50 +0530

    Fetcher

--- java_fundamentals/src/main/java/org/example/multithreading/BasicMultiThreading.java
+++ java_fundamentals/src/main/java/org/example/multithreading/BasicMultiThreading.java
@@ -1,10 +1,8 @@
 package org.example.multithreading;
 
-
 public class BasicMultiThreading {
 
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void getDataInSync() throws InterruptedException {
         long start = System.currentTimeMillis();
         Fetcher fetcher1 = new Fetcher();
         fetcher1.fetch();
@@ -14,6 +12,34 @@ public class BasicMultiThreading {
         System.out.println("Time taken by main thread - " + (end - start) / 1000 + "secs");
     }
 
+    public static void getDataInAsync() throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        Thread caller = new Thread(() -> {
+            Fetcher fetcher1 = new Fetcher();
+            try {
+                fetcher1.fetch();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread caller2 = new Thread(() -> {
+            Fetcher fetcher2 = new Fetcher();
+            try {
+                fetcher2.fetch();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        caller.start();
+        caller2.start();
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken by main thread - " + (end - start) / 1000 + "secs");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        getDataInAsync();
+    }
 }
 
 
